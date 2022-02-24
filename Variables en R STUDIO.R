d92e6953bf4b09e53d7427da4e11844d82cfedd1
Eliud Morales <96699956+EliudMorales010@users.noreply.github.com>
2022-02-24 16:29:05 -0600

    Delete Variables en R STUDIO.R

--- Variables en R STUDIO.R
+++
@@ -1,7 +0,0 @@
-# R es una calculadora, podemos sumar, restar, multiplicar y dividir
-
-# Las operaciones aritmeticas nos ayudan a:
-# Crear nuevas variables para el analisis
-# Realizar calculos complejos
-
-# Resolver este acertijo
